ba0fafb8ae7441c9482a0e3960f3e481ddb2a6cf
yuwenming <yuwenming@nucarf.com>
2019-04-23 11:19:32 +0800

    极光推送  友盟统计，友盟分享 base 添加

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.myapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerMessageReceiver();  // used for receive msg
+
+    }
+    public static boolean isForeground = false;
+
+    //for receive customer msg from jpush server
+    private MessageReceiver mMessageReceiver;
+    public static final String MESSAGE_RECEIVED_ACTION = "com.example.jpushdemo.MESSAGE_RECEIVED_ACTION";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_EXTRAS = "extras";
+
+    public void registerMessageReceiver() {
+        mMessageReceiver = new MessageReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+        filter.addAction(MESSAGE_RECEIVED_ACTION);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, filter);
+    }
+
+    public class MessageReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            try {
+                if (MESSAGE_RECEIVED_ACTION.equals(intent.getAction())) {
+                    String messge = intent.getStringExtra(KEY_MESSAGE);
+                    String extras = intent.getStringExtra(KEY_EXTRAS);
+                    StringBuilder showMsg = new StringBuilder();
+                    showMsg.append(KEY_MESSAGE + " : " + messge + "\n");
+//                    if (!ExampleUtil.isEmpty(extras)) {
+//                        showMsg.append(KEY_EXTRAS + " : " + extras + "\n");
+//                    }
+//                    setCostomMsg(showMsg.toString());
+                }
+            } catch (Exception e){
+            }
+        }
     }
 }
