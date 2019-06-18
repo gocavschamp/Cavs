a5dc97716b98d22a31315ea47059dbf5127c1063
yuwenming <yuwenming@nucarf.com>
2019-06-18 16:51:44 +0800

    修改v1.1.2

--- app/src/main/java/com/example/myapp/login/view/LoginActivity.java
+++ app/src/main/java/com/example/myapp/login/view/LoginActivity.java
@@ -12,5 +12,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // v1.1.2
     }
 }
