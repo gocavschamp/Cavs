e108820b7df7f9983f6d40a3f23ca54fab87002a
yuwenming <yuwenming@nucarf.com>
2020-02-18 13:38:26 +0800

    下载文件utils

--- app/src/main/java/com/example/myapp/api/AppService.java
+++ app/src/main/java/com/example/myapp/api/AppService.java
@@ -10,7 +10,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FieldMap;
@@ -20,6 +22,7 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
+import retrofit2.http.Streaming;
 import retrofit2.http.Url;
 
 public interface AppService {
@@ -48,5 +51,13 @@ public interface AppService {
     @GET
 //好看视频
     Observable<BaseResult<VideoListData>> getVideoList(@Url String url);
+
+//    https://gss0.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/zhidao/wh%3D600%2C800/sign=9933924977310a55c471d6f287756f91/314e251f95cad1c8c983f49d713e6709c93d514b.jpg
+
+    //使用Streaming 方式 Retrofit 不会一次性将ResponseBody 读取进入内存，否则文件很多容易OOM
+    @GET
+    @Streaming
+    Flowable<ResponseBody> downloadFile(@Url String url);  //返回值使用 ResponseBody 之后会对ResponseBody 进行读取
+
 
 }
--- app/src/main/java/com/example/myapp/homepage/MineFragment.java
+++ app/src/main/java/com/example/myapp/homepage/MineFragment.java
@@ -1,17 +1,27 @@
 package com.example.myapp.homepage;
 
+import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.myapp.BuildConfig;
 import com.example.myapp.R;
+import com.example.myapp.utils.download.DownloadHelper;
+import com.example.myapp.utils.download.DownloadListener;
+import com.nucarf.base.retrofit.RetrofitConfig;
 import com.nucarf.base.ui.BaseLazyFragment;
 import com.nucarf.base.utils.GlideUtils;
 import com.nucarf.base.widget.RoundImageView;
 import com.nucarf.base.widget.StarBar;
 
+import java.io.File;
+
 import butterknife.BindView;
 
-public class MineFragment extends BaseLazyFragment {
+public class MineFragment extends BaseLazyFragment implements DownloadListener {
 
 
     @BindView(R.id.star_bar)
@@ -20,6 +30,8 @@ public class MineFragment extends BaseLazyFragment {
     TextView tvInfo;
     @BindView(R.id.round_iv_head)
     RoundImageView roundIvHead;
+    @BindView(R.id.iv_pic)
+    ImageView ivPic;
 
     public MineFragment() {
     }
@@ -44,7 +56,7 @@ public class MineFragment extends BaseLazyFragment {
         stringBuffer.append(BuildConfig.FLAVOR + "  flavor\n");
         stringBuffer.append(BuildConfig.VERSION_CODE + "  code\n");
         stringBuffer.append(BuildConfig.VERSION_NAME + "  name\n");
-        stringBuffer.append(getString(R.string.app_name )+ "  app name\n");
+        stringBuffer.append(getString(R.string.app_name) + "  app name\n");
         stringBuffer.append(getString(R.string.app_welcome) + "  app welcome\n");
         stringBuffer.append(getString(R.string.age) + "  age\n");
         tvInfo.setText(stringBuffer);
@@ -52,9 +64,43 @@ public class MineFragment extends BaseLazyFragment {
 
     }
 
+    // 初始化
+    private DownloadHelper mDownloadHelper = new DownloadHelper(RetrofitConfig.TEST_HOST_URL, this);
+
+    @SuppressLint("CheckResult")
     @Override
     protected void initView() {
+        String url = "https://gss0.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/zhidao/wh%3D600%2C800/sign=9933924977310a55c471d6f287756f91/314e251f95cad1c8c983f49d713e6709c93d514b.jpg";
+//        String url = "http://fenghuoyunji.jb51.net:81/201906/tools/OkHttpJar_jb51.rar";
+        Log.e("TAG", "下载文件 start");
+
+        // 调用方法
+        mDownloadHelper.downloadFile(url, mActivity.getCacheDir() + File.separator + "/member", "nucarf.jpg");
 
     }
 
+    @Override
+    public void onStartDownload() {
+        Log.e("TAG", "下载文件 start");
+
+    }
+
+    @Override
+    public void onProgress(int progress) {
+        Log.e("TAG", "下载文件 start" + progress);
+
+    }
+
+    @Override
+    public void onFinishDownload(File file) {
+        Log.e("TAG", "下载文件 finish   " + file.getName() + "--\n" + file.getAbsolutePath());
+        Glide.with(mActivity).load(Uri.fromFile(file)).into(ivPic);
+
+    }
+
+    @Override
+    public void onFail(Throwable ex) {
+        Log.e("TAG", "下载文件 failed");
+
+    }
 }
