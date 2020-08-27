3f10a8022408786d8aa2221baa6c2298a57f0f43
yuwenming <yuwenming@nucarf.com>
2020-08-27 18:07:34 +0800

    hupu gift

--- app/src/main/java/com/example/myapp/widget/HupuGiftView.java
+++ app/src/main/java/com/example/myapp/widget/HupuGiftView.java
@@ -10,6 +10,10 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Gravity;
@@ -62,6 +66,11 @@ public class HupuGiftView extends RelativeLayout {
     private boolean isShow = false;
     private View rl_content;
     private boolean isShowing = false;
+    private RelativeLayout rl_content2;
+    private TextView tv2Flag;
+    private TextView tv2Gift1;
+    private TextView tv2Gift2;
+    private TextView tv2Gift3;
 
     public HupuGiftView(Context context) {
         this(context, null, 0);
@@ -113,16 +122,57 @@ public class HupuGiftView extends RelativeLayout {
 //        team1FlagPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 //        hupuViewBgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         View inflate = View.inflate(context, R.layout.hupu_gift_view, null);
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.addRule(Gravity.RIGHT);
-        inflate.setLayoutParams(layoutParams);
-//        ButterKnife.bind(inflate);
-        addView(inflate);
-        tvFlag = inflate.findViewById(R.id.tv_flag);
         rl_content = inflate.findViewById(R.id.rl_content);
+        tvFlag = inflate.findViewById(R.id.tv_flag);
         tvGift1 = inflate.findViewById(R.id.tv_gift1);
         tvGift2 = inflate.findViewById(R.id.tv_gift2);
         tvGift3 = inflate.findViewById(R.id.tv_gift3);
+        View inflate1 = View.inflate(context, R.layout.hupu_gift_view2, null);
+        rl_content2 = inflate1.findViewById(R.id.rl_content2);
+        tv2Flag = inflate1.findViewById(R.id.tv2_flag);
+        tv2Gift1 = inflate1.findViewById(R.id.tv2_gift1);
+        tv2Gift2 = inflate1.findViewById(R.id.tv2_gift2);
+        tv2Gift3 = inflate1.findViewById(R.id.tv2_gift3);
+        tv2Flag.setText(team2_flag);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(Gravity.RIGHT);
+        layoutParams.addRule(ALIGN_PARENT_RIGHT);
+        inflate.setLayoutParams(layoutParams);
+        RelativeLayout.LayoutParams layoutParams1 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams1.addRule(ALIGN_PARENT_RIGHT);
+////        inflate1.setLayoutParams(layoutParams1);
+////        ButterKnife.bind(inflate);
+        addView(inflate);
+        LayoutParams layoutParams2 = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams2.addRule(BELOW, R.id.rl_content);
+        layoutParams2.addRule(ALIGN_PARENT_RIGHT);
+//        addView(inflate1, layoutParams2);
+
+        tvFlag.setText(team1_flag);
+        GradientDrawable gradientDrawable = new GradientDrawable();
+        gradientDrawable.setColor(team1_bg_color);
+        gradientDrawable.setShape(GradientDrawable.OVAL);
+        gradientDrawable.setCornerRadius(team_size);
+        GradientDrawable gradientDrawable1 = new GradientDrawable();
+        gradientDrawable1.setColor(team2_bg_color);
+        gradientDrawable1.setShape(GradientDrawable.OVAL);
+        gradientDrawable1.setCornerRadius(team_size);
+
+        GradientDrawable gradientDrawable2 = new GradientDrawable();
+        gradientDrawable2.setColor(team1_bg_color);
+        gradientDrawable2.setShape(GradientDrawable.OVAL);
+        gradientDrawable2.setCornerRadius(team_size);
+        GradientDrawable gradientDrawable3 = new GradientDrawable();
+        gradientDrawable3.setColor(team1_bg_color);
+        gradientDrawable3.setShape(GradientDrawable.OVAL);
+        gradientDrawable3.setCornerRadius(team_size);
+
+        tvFlag.setBackground(gradientDrawable);
+//        tv2Flag.setBackground(gradientDrawable1);
+
+        tvGift1.setBackground(gradientDrawable1);
+        tvGift2.setBackground(gradientDrawable2);
+        tvGift3.setBackground(gradientDrawable3);
         tvFlag.setOnClickListener((v -> {
             if (!isShow) {
                 showGift();
@@ -134,6 +184,7 @@ public class HupuGiftView extends RelativeLayout {
 
     }
 
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -143,8 +194,8 @@ public class HupuGiftView extends RelativeLayout {
         if (isShowing) {
             return;
         }
-        int mWidth = tvFlag.getWidth() + tvGift1.getWidth() * 3 + 10 * 4;
-        ValueAnimator valueAnimator = ValueAnimator.ofFloat(tvFlag.getWidth(), mWidth+30);
+        int mWidth = tvFlag.getWidth() + tvGift1.getWidth() * 3 + 40;
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(tvFlag.getWidth(), mWidth / 2, mWidth * 3 / 4, mWidth);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
@@ -155,13 +206,13 @@ public class HupuGiftView extends RelativeLayout {
         });
 
         ObjectAnimator rotation = ObjectAnimator.ofFloat(tvGift1, "rotation", 0, -360);
-        ObjectAnimator translationX = ObjectAnimator.ofFloat(tvGift1, "translationX", 0, -(tvFlag.getWidth() + 10));
+        ObjectAnimator translationX = ObjectAnimator.ofFloat(tvGift1, "translationX", 0, -(tvGift1.getWidth() + 10));
         ObjectAnimator rotation1 = ObjectAnimator.ofFloat(tvGift2, "rotation", 0, -360);
-        ObjectAnimator translationX1 = ObjectAnimator.ofFloat(tvGift2, "translationX", 0, -(tvFlag.getWidth() + tvGift1.getWidth() + 20));
+        ObjectAnimator translationX1 = ObjectAnimator.ofFloat(tvGift2, "translationX", 0, -(tvGift1.getWidth() + tvGift1.getWidth() + 20));
         ObjectAnimator rotation2 = ObjectAnimator.ofFloat(tvGift3, "rotation", 0, -360);
-        ObjectAnimator translationX2 = ObjectAnimator.ofFloat(tvGift3, "translationX", 0, -(tvFlag.getWidth() + tvGift1.getWidth() * 2 + 30));
+        ObjectAnimator translationX2 = ObjectAnimator.ofFloat(tvGift3, "translationX", 0, -(tvGift1.getWidth() + tvGift1.getWidth() * 2 + 30));
         AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.setDuration(400);
+        animatorSet.setDuration(200);
         animatorSet.addListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
@@ -170,6 +221,8 @@ public class HupuGiftView extends RelativeLayout {
 
             @Override
             public void onAnimationEnd(Animator animation) {
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams((int) mWidth, ViewGroup.LayoutParams.WRAP_CONTENT);
+                rl_content.setLayoutParams(layoutParams);
                 isShow = true;
                 isShowing = false;
             }
@@ -195,8 +248,8 @@ public class HupuGiftView extends RelativeLayout {
         if (isShowing) {
             return;
         }
-        int mWidth = tvFlag.getWidth() + tvGift1.getWidth() * 3 + 10 * 4;
-        ValueAnimator valueAnimator = ValueAnimator.ofFloat(mWidth+60, tvFlag.getMeasuredWidth());
+        int mWidth = tvFlag.getWidth() + tvGift1.getWidth() * 3 + 40;
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(mWidth, mWidth * 3 / 4, mWidth / 2, tvFlag.getWidth());
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
@@ -206,13 +259,13 @@ public class HupuGiftView extends RelativeLayout {
             }
         });
         ObjectAnimator rotation = ObjectAnimator.ofFloat(tvGift1, "rotation", 0, 360);
-        ObjectAnimator translationX = ObjectAnimator.ofFloat(tvGift1, "translationX", -(tvFlag.getWidth()- tvGift1.getWidth() -10), 0);
+        ObjectAnimator translationX = ObjectAnimator.ofFloat(tvGift1, "translationX", -(tvGift1.getWidth() + 10), 0);
         ObjectAnimator rotation1 = ObjectAnimator.ofFloat(tvGift2, "rotation", 0, 360);
-        ObjectAnimator translationX1 = ObjectAnimator.ofFloat(tvGift2, "translationX", (tvFlag.getWidth() - tvGift1.getWidth()*2 -10), 0);
+        ObjectAnimator translationX1 = ObjectAnimator.ofFloat(tvGift2, "translationX", -(tvGift1.getWidth() + tvGift1.getWidth() + 20), 0);
         ObjectAnimator rotation2 = ObjectAnimator.ofFloat(tvGift3, "rotation", 0, 360);
-        ObjectAnimator translationX2 = ObjectAnimator.ofFloat(tvGift3, "translationX", (tvFlag.getWidth() - tvGift1.getWidth() *3 - 20), 0);
+        ObjectAnimator translationX2 = ObjectAnimator.ofFloat(tvGift3, "translationX", -(tvGift1.getWidth() + tvGift1.getWidth() * 2 + 30), 0);
         AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.setDuration(400);
+        animatorSet.setDuration(200);
         animatorSet.playTogether(rotation, rotation1, rotation2, translationX, translationX1, translationX2, valueAnimator);
         animatorSet.setInterpolator(new LinearInterpolator());
         animatorSet.addListener(new Animator.AnimatorListener() {
@@ -244,7 +297,15 @@ public class HupuGiftView extends RelativeLayout {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
-
+        Log.e("hupu--count-",getChildCount()+"");
+//        for (int i = 0; i < getChildCount(); i++) {
+//            View childAt = getChildAt(i);
+//            if (i == 0) {
+//                childAt.layout(r - childAt.getWidth(), t, r, childAt.getHeight());
+//            } else {
+//                childAt.layout(r - childAt.getWidth(), t + childAt.getHeight(), r, t + childAt.getHeight() * 2);
+//            }
+//        }
     }
 
     @Override
